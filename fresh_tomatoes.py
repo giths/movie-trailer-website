3b23a79887759fdd66088570947a9f28928f2161
skomidi <skomidi@yahoo.com>
2015-06-11 11:03:05 -0700

    Fixed style as per PEP8

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -2,17 +2,25 @@ import webbrowser
 import os
 import re
 
+# bootstrap url prefix
+bstrap_url_pre = "https://netdna.bootstrapcdn.com/bootstrap/3.1.0"
+
 # Styles and scripting for the page
-main_page_head = '''
+main_page_head = ('''
 <head>
     <meta charset="utf-8">
     <title>Fresh Tomatoes!</title>
 
+''' + '''
+
     <!-- Bootstrap 3 -->
-    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="{bstrap_url_pre}/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{bstrap_url_pre}/css/bootstrap-theme.min.css">
     <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
-    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+    <script src="{bstrap_url_pre}/js/bootstrap.min.js"></script>
+
+'''.format(bstrap_url_pre=bstrap_url_pre) + '''
+
     <style type="text/css" media="screen">
         body {
             padding-top: 80px;
@@ -68,23 +76,33 @@ main_page_head = '''
         }
     </style>
     <script type="text/javascript" charset="utf-8">
+
         // Pause the video when the modal is closed
-        $(document).on('click', '.hanging-close, .modal-backdrop, .modal', function (event) {
-            // Remove the src so the player itself gets removed, as this is the only
-            // reliable way to ensure the video stops playing in IE
-            $("#trailer-video-container").empty();
-        });
+        $(document).on(
+            'click',
+            '.hanging-close, .modal-backdrop, .modal',
+            function (event) {
+                // Remove the src so the player itself gets removed, as
+                // this is the only reliable way to ensure the video
+                // stops playing in IE
+                $("#trailer-video-container").empty(); });
+
         // Start playing the video whenever the trailer modal is opened
-        $(document).on('click', '.movie-tile', function (event) {
-            var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
-            var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
-            $("#trailer-video-container").empty().append($("<iframe></iframe>", {
-              'id': 'trailer-video',
-              'type': 'text-html',
-              'src': sourceUrl,
-              'frameborder': 0
-            }));
-        });
+        $(document).on(
+            'click',
+            '.movie-tile',
+            function (event) {
+                var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
+                var sourceUrl = 'http://www.youtube.com/embed/' +
+                                trailerYouTubeId +
+                                '?autoplay=1&html5=1';
+                $("#trailer-video-container").empty().append(
+                    $("<iframe></iframe>",
+                      { 'id': 'trailer-video',
+                        'type': 'text-html',
+                        'src': sourceUrl,
+                        'frameborder': 0 }));   });
+
         // Animate in the movies when the page loads
         $(document).ready(function () {
           $('.movie-tile').hide().first().show("fast", function showNext() {
@@ -93,7 +111,11 @@ main_page_head = '''
         });
     </script>
 </head>
-'''
+''')
+
+img_src = ("https://lh5.ggpht.com/"
+           "v4-628SilF0HtHuHdu5EzxD7WRqOrrTIDi_MhEG6_qkNtUK5Wg7KPkofp_"
+           "VJoF7RS2LhxwEFCO1ICHZlc-o_=s0#w=24&h=24")
 
 # The main page layout and title bar
 main_page_content = '''
@@ -104,15 +126,16 @@ main_page_content = '''
     <div class="modal" id="trailer">
       <div class="modal-dialog">
         <div class="modal-content">
-          <a href="#" class="hanging-close" data-dismiss="modal" aria-hidden="true">
-            <img src="https://lh5.ggpht.com/v4-628SilF0HtHuHdu5EzxD7WRqOrrTIDi_MhEG6_qkNtUK5Wg7KPkofp_VJoF7RS2LhxwEFCO1ICHZlc-o_=s0#w=24&h=24"/>
+          <a href="#" class="hanging-close"
+                data-dismiss="modal" aria-hidden="true">
+            <img src="{img_src}"/>
           </a>
           <div class="scale-media" id="trailer-video-container">
           </div>
         </div>
       </div>
     </div>
-    
+
     <!-- Main Page Content -->
     <div class="container">
       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -124,15 +147,17 @@ main_page_content = '''
       </div>
     </div>
     <div class="container">
-      {movie_tiles}
+      {{movie_tiles}}
     </div>
   </body>
 </html>
-'''
+'''.format(img_src=img_src)
 
 # A single movie entry html template
 movie_tile_content = '''
-<div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
+<div class="col-md-6 col-lg-4 movie-tile text-center"
+ data-trailer-youtube-id="{trailer_youtube_id}"
+ data-toggle="modal" data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
     <span class="overlay"> <h2> Director <h3>{movie_director}</h3></h2></span>
     <h2>{movie_title}</h2>
@@ -140,14 +165,22 @@ movie_tile_content = '''
 </div>
 '''
 
+
 def create_movie_tiles_content(movies):
     # The HTML content for this section of the page
     content = ''
     for movie in movies:
+
         # Extract the youtube ID from the url
-        youtube_id_match = re.search(r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
-        youtube_id_match = youtube_id_match or re.search(r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
-        trailer_youtube_id = youtube_id_match.group(0) if youtube_id_match else None
+
+        youtube_id_match = re.search(
+                r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
+
+        youtube_id_match = youtube_id_match or \
+            re.search(r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
+
+        trailer_youtube_id = youtube_id_match.group(0) \
+            if youtube_id_match else None
 
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
@@ -159,17 +192,20 @@ def create_movie_tiles_content(movies):
         )
     return content
 
+
 def open_movies_page(movies):
-  # Create or overwrite the output file
-  output_file = open('fresh_tomatoes.html', 'w')
+    # Create or overwrite the output file
+    output_file = open('fresh_tomatoes.html', 'w')
 
-  # Replace the placeholder for the movie tiles with the actual dynamically generated content
-  rendered_content = main_page_content.format(movie_tiles=create_movie_tiles_content(movies))
+    # Replace the placeholder for the movie tiles with the actual dynamically
+    # generated content
+    rendered_content = main_page_content.format(
+        movie_tiles=create_movie_tiles_content(movies))
 
-  # Output the file
-  output_file.write(main_page_head + rendered_content)
-  output_file.close()
+    # Output the file
+    output_file.write(main_page_head + rendered_content)
+    output_file.close()
 
-  # open the output file in the browser
-  url = os.path.abspath(output_file.name)
-  webbrowser.open('file://' + url, new=2) # open in a new tab, if possible
+    # open the output file in the browser
+    url = os.path.abspath(output_file.name)
+    webbrowser.open('file://' + url, new=2)  # open in a new tab, if possible
--- media.py
+++ media.py
@@ -1,11 +1,10 @@
 class Movie(object):
     """ This class stores information about a movie """
 
-    def __init__(self, title,  poster_image, trailer_youtube, 
+    def __init__(self, title,  poster_image, trailer_youtube,
                  rating, director):
-        self.title               = title
-        self.poster_image_url    = poster_image
+        self.title = title
+        self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-        self.rating              = rating
-        self.director            = director
-
+        self.rating = rating
+        self.director = director
